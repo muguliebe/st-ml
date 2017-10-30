0c4b894fa4678640780fb3cd065f4399db433c17
zany <muguliebe@gmail.com>
2017-10-30 16:15:00 +0900

    π

--- 05-dl/03-var.py
+++ 05-dl/03-var.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
 
+# tmp
+dir(tf)
+
 # declare constants
 a = tf.constant(120, name='a')
 b = tf.constant(130, name='b')
 c = tf.constant(140, name='c')
 
 # declare variables
-v = tf.Variables(0, name='v')
+v = tf.Variable(0, name='v')
 
 # define graph of data flow
 calc_op = a + b + c
