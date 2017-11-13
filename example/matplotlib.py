bc714e13157f43bd96c39c074e98b6d14d56cb51
zany <muguliebe@gmail.com>
2017-11-13 18:01:32 +0900

    ˆ

--- example/matplotlib.py
+++ example/matplotlib.py
@@ -6,12 +6,6 @@ import matplotlib.pyplot as plt
 plt.ioff()
 for i in range(3):
     plt.plot(np.random.rand(10))
-#plt.show()
-
-import os
-directory = './data'
-if not os.path.exists(directory):
-    os.makedirs(directory)
 
 # plt.savefig('data/matplotlib.png')  #savefig, don't show
 plt.show()
--- util/zutil.py
+++ util/zutil.py
@@ -1,3 +1,12 @@
+import os
+
+
 def test():
     print('test')
     pass
+
+
+def makeDataDicrectory():
+    directory = '../data'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
